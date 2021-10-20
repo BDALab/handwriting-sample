92ac4b25d8f8696d7dd44dd8bf33c022aaa72f60
DzanMucha <muchajano@phd.feec.vutbr.cz>
2021-10-20 10:45:24 +0200

    bit of refactoring

--- examples/tests/test_sample_class.py
+++ examples/tests/test_sample_class.py
@@ -1,11 +1,11 @@
 import numpy as np
-from handwriting_sample.sample import Sample
+from handwriting_sample.handwriting_sample import HandwritingSample
 from handwriting_sample.transformers import Transformer
 
 from pprint import pprint
 
 additional_meta_data = {
-    "column_names": [Sample.TILT, Sample.AZIMUTH, Sample.AX_X, Sample.PRESSURE, Sample.PEN_STATUS, Sample.TIME, Sample.AX_Y],
+    "column_names": [HandwritingSample.TILT, HandwritingSample.AZIMUTH, HandwritingSample.AX_X, HandwritingSample.PRESSURE, HandwritingSample.PEN_STATUS, HandwritingSample.TIME, HandwritingSample.AX_Y],
     "protocol_id": "pd_protocol_2018",
     "device_type": "Wacom Cinitq",
     "device_driver": "2.1.0",
@@ -23,43 +23,44 @@ additional_meta_data = {
 
 def test_read_sample_svc():
 
-    sample = Sample.from_svc("../svc_data/signal.svc")
+    sample = HandwritingSample.from_svc("../svc_data/signal.svc")
     print(sample)
-    print(sample.time)
 
     assert sample
 
 
-def test_read_sample_svc_new():
-    column_names = ['y', 'x', 'time', 'pen_status', 'azimuth', 'tilt', 'pressure']
+def test_read_sample_svc_and_add_meta_data():
+    sample = HandwritingSample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
+    print("original metadata: ")
+    pprint(sample.meta_data)
 
-    sample = Sample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc", column_names=column_names)
-    print(sample)
-    # pprint(sample.meta_data)
-    # sample.add_meta_data(additional_meta_data)
-    # pprint(sample.meta_data)
-    # print(sample._data)
+    sample.add_meta_data(additional_meta_data)
+    print("\n\nnew metadata: ")
+    pprint(sample.meta_data)
 
     assert sample
 
 
 def test_read_sample_json():
-    sample = Sample.from_json("../json_data/signal.json")
+    sample = HandwritingSample.from_json("../json_data/signal.json")
     print(sample)
 
     assert sample
 
 
-def test_read_sample_pandas():
+def test_read_sample_pandas_with_different_column_order():
     # get _data in pd.Dataframe
-    sample = Sample.from_json("../json_data/signal.json")
+    sample = HandwritingSample.from_json("../json_data/signal.json")
     data_df = sample._data
 
     # Reorder _data (to check if ordering is working)
-    data_df = data_df[[Sample.TILT, Sample.AZIMUTH, Sample.AX_X, Sample.PRESSURE, Sample.PEN_STATUS, Sample.TIME, Sample.AX_Y]]
+    data_df = data_df[[HandwritingSample.TILT, HandwritingSample.AZIMUTH,
+                       HandwritingSample.AX_X, HandwritingSample.PRESSURE,
+                       HandwritingSample.PEN_STATUS, HandwritingSample.TIME,
+                       HandwritingSample.AX_Y]]
 
     # Read _data from DataFrame
-    df_sample = Sample.from_pandas(data_df)
+    df_sample = HandwritingSample.from_pandas(data_df)
     print(df_sample._data)
 
     assert df_sample
@@ -76,44 +77,66 @@ def test_from_array():
 
     column_names = ['pen_status', 'y', 'x', 'time', 'azimuth', 'tilt', 'pressure']
 
-    sample = Sample.from_array(array, column_names=column_names)
+    sample = HandwritingSample.from_array(array, column_names=column_names)
     print(sample)
 
     assert sample
 
 
-def test_validate_sample_pandas():
+def test_validate_missing_columns():
+
     # get _data in pd.Dataframe
-    sample = Sample.from_json("../json_data/signal.json")
-    data_df = sample._data
+    sample = HandwritingSample.from_json("../json_data/signal.json")
+    data_df = sample.get_df_sample_accessible_data()
 
-    # Drop column
-    # data_df = data_df.drop(columns=[Sample.TIME, Sample.AX_X])
+    # Drop columns
+    data_df = data_df.drop(columns=[HandwritingSample.TIME])
 
-    # # Add columns
-    # data_df['Biceps'] = 1
-    # data_df['HeadDiameter'] = 1
-    #
-    # # Set column to null
+    # Read corrupted _data
+    try:
+        df_sample = HandwritingSample.from_pandas(data_df)
+        assert False
+    except Exception as ex:
+        print(ex)
+        assert ex
+
+
+def test_validate_additional_columns():
+    # get _data in pd.Dataframe
+    sample = HandwritingSample.from_json("../json_data/signal.json")
+    data_df = sample.get_df_sample_accessible_data()
+
+    # Add columns
+    data_df['Biceps'] = 1
+
+    # Read corrupted _data
+    try:
+        df_sample = HandwritingSample.from_pandas(data_df)
+        assert False
+    except Exception as ex:
+        print(ex)
+        assert ex
+
+
+def test_validate_time_is_none():
+    # get _data in pd.Dataframe
+    sample = HandwritingSample.from_json("../json_data/signal.json")
+    data_df = sample.get_df_sample_accessible_data()
+
+    # Set column to null
     data_df['time'] = None
 
     # Read corrupted _data
-    df_sample = Sample.from_pandas(data_df)
-
-    print(df_sample)
-
-
-def test_read_sample_array():
-    sample = Sample.from_svc("../svc_data/signal.svc")
-    print(sample._data)
-    print(f"X: {sample.x}")
-    print(f"Time: {sample.time}")
-
-    assert sample
+    try:
+        df_sample = HandwritingSample.from_pandas(data_df)
+        assert False
+    except Exception as ex:
+        print(ex)
+        assert ex
 
 
 def test_split_movements():
-    sample = Sample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
+    sample = HandwritingSample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
 
     on_surface_data = sample.get_on_surface_data()
     print(f"On Surface: {on_surface_data}")
@@ -125,7 +148,7 @@ def test_split_movements():
 
 
 def test_get_strokes():
-    sample = Sample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
+    sample = HandwritingSample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
     strokes = sample.get_strokes()
     print(f"Number of strokes: {len(strokes)}")
 
@@ -133,7 +156,7 @@ def test_get_strokes():
 
 
 def test_get_on_surface_strokes():
-    sample = Sample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
+    sample = HandwritingSample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
     strokes = sample.get_on_surface_strokes()
     print(f"Number of strokes on surface: {len(strokes)}")
 
@@ -141,35 +164,32 @@ def test_get_on_surface_strokes():
 
 
 def test_get_in_air_strokes():
-    sample = Sample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
+    sample = HandwritingSample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
     strokes = sample.get_in_air_strokes()
     print(f"Number of strokes in air: {len(strokes)}")
 
     assert sample
 
 
-def test_store_data():
-    # sample = Sample(data_source="signal.json")
-    sample = Sample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
+def test_store_data_to_json():
+    sample = HandwritingSample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
 
-    sample.to_json("./")
+    sample.to_json("../")
 
     assert sample
 
 
-def test_store_raw_data():
-    # sample = Sample(data_source="signal.json")
-    sample = Sample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
+def test_store_raw_data_to_svc():
+    sample = HandwritingSample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
 
-    sample.to_svc("./", original_data=True, file_name="original_data")
+    sample.to_svc("../", original_data=True, file_name="original_data")
 
     assert sample
 
 
 def test_load_sample_with_transformation():
 
-    sample = Sample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
-    # print(sample)
+    sample = HandwritingSample.from_svc("../svc_data/jack_18-07-2014_M_0006_Doe_12-10-2020.svc")
 
     sample = Transformer.transform_handwriting_units(sample, angles_to_degrees=True)
     print(f"X: {sample.x}")
