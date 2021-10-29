a85fdf5bd76b6c9cd45108fd6b39e0fd9c47d31f
Zoltán Galáž <xgalaz00@gmail.com>
2021-10-29 09:19:20 +0200

    added exceptions from visualizer package to global exceptions

--- handwriting_sample/exceptions.py
+++ handwriting_sample/exceptions.py
@@ -2,3 +2,4 @@ from handwriting_sample.reader.exceptions import *
 from handwriting_sample.writer.exceptions import *
 from handwriting_sample.transformer.exceptions import *
 from handwriting_sample.validator.exceptions import *
+from handwriting_sample.visualizer.exceptions import *
