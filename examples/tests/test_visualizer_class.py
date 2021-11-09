c6274df9e773c4f4ddd1265e13333f09f89a2cb1
DzanMucha <muchajano@phd.feec.vutbr.cz>
2021-11-09 10:52:09 +0000

    extend visualiser for all modalities plots, strokes and in air data plot

--- examples/tests/test_visualizer_class.py
+++ examples/tests/test_visualizer_class.py
@@ -18,3 +18,63 @@ def test_plot_in_air():
 
     assert sample
 
+
+def test_plot_separate_movements():
+
+    sample = HandwritingSample.from_svc(svc_file)
+    sample.visualizer.plot_separate_movements(sample)
+    print(sample)
+
+    assert sample
+
+
+def test_plot_strokes():
+
+    sample = HandwritingSample.from_svc(svc_file)
+    sample.visualizer.plot_strokes(sample)
+    print(sample)
+
+    assert sample
+
+
+def test_plot_x():
+    sample = HandwritingSample.from_svc(svc_file)
+    sample.visualizer.plot_line(sample.x, x_label='sample', y_label='x [mm]')
+
+    assert sample
+
+
+def test_plot_y():
+    sample = HandwritingSample.from_svc(svc_file)
+    sample.visualizer.plot_line(sample.y, x_label='sample', y_label='y [mm]')
+
+    assert sample
+
+
+def test_plot_time():
+    sample = HandwritingSample.from_svc(svc_file)
+    sample.visualizer.plot_line(sample.time, x_label='sample', y_label='time [s]')
+
+    assert sample
+
+
+def test_plot_azimuth():
+    sample = HandwritingSample.from_svc(svc_file)
+    sample.visualizer.plot_line(sample.azimuth, x_label='sample', y_label='azimuth [°]')
+
+    assert sample
+
+
+def test_plot_tilt():
+    sample = HandwritingSample.from_svc(svc_file)
+    sample.visualizer.plot_line(sample.tilt, x_label='sample', y_label='tilt [°]')
+
+    assert sample
+
+
+def test_plot_all_data():
+    sample = HandwritingSample.from_svc(svc_file)
+    sample.visualizer.plot_all_modalities(sample)
+
+    assert sample
+
--- handwriting_sample/visualizer/interface.py
+++ handwriting_sample/visualizer/interface.py
@@ -1,10 +1,9 @@
-import plotly.graph_objects as go
-import seaborn as sns
-import matplotlib
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 
 from handwriting_sample.base import HandwritingDataBase
+
+#TODO: First fast version, need to be refactored to use plotly
 
 
 class HandwritingSampleVisualizer(HandwritingDataBase):
@@ -28,11 +27,32 @@ class HandwritingSampleVisualizer(HandwritingDataBase):
         ax, plot = self._plot_strokes(sample,
                                       x_label=x_label,
                                       y_label=y_label,
+                                      on_surface_movement=True,
                                       on_surface_color='darkblue',
                                       save_as=save_as)
         return ax, plot
 
     def plot_in_air_movement(self, sample, x_label=None, y_label=None, save_as=None):
+        """
+        Plot task with in air movement
+
+        :param sample: instance of handwriting sample
+        :type sample: HandwritingSample
+        :param x_label: label of X axis
+        :param y_label: label of Y axis
+        :param save_as: setup save location if you want to save a file
+        """
+        # Plot task with in air data
+        ax, plot = self._plot_strokes(sample,
+                                      x_label=x_label,
+                                      y_label=y_label,
+                                      in_air_movement=True,
+                                      in_air_color='plum',
+                                      save_as=save_as)
+
+        return ax, plot
+
+    def plot_separate_movements(self, sample, x_label=None, y_label=None, save_as=None):
         """
         Plot task with separate movements
 
@@ -47,22 +67,45 @@ class HandwritingSampleVisualizer(HandwritingDataBase):
                                       x_label=x_label,
                                       y_label=y_label,
                                       in_air_movement=True,
+                                      on_surface_movement=True,
                                       on_surface_color='darkblue',
                                       in_air_color='plum',
                                       save_as=save_as)
 
         return ax, plot
 
-    def _plot_strokes(self,
-                      sample,
-                      x_label=None,
-                      y_label=None,
-                      in_air_movement=False,
-                      on_surface_color=None,
-                      in_air_color=None,
-                      fig_show=True,
-                      save_as=None,
-                      fig_kwargs=None):
+    def plot_strokes(self, sample, x_label=None, y_label=None, save_as=None):
+        """
+        Plot task separate strokes
+
+        :param sample: instance of handwriting sample
+        :type sample: HandwritingSample
+        :param x_label: label of X axis
+        :param y_label: label of Y axis
+        :param save_as: setup save location if you want to save a file
+        """
+        # Plot task with in air data
+        ax, plot = self._plot_strokes(sample,
+                                      x_label=x_label,
+                                      y_label=y_label,
+                                      in_air_movement=True,
+                                      on_surface_movement=True,
+                                      save_as=save_as)
+
+        return ax, plot
+
+    def _plot_strokes(
+            self,
+            sample,
+            x_label=None,
+            y_label=None,
+            in_air_movement=False,
+            on_surface_movement=False,
+            on_surface_color=None,
+            in_air_color=None,
+            fig_show=True,
+            save_as=None,
+            fig_kwargs=None):
         """
         Plot line plots for input data
 
@@ -71,6 +114,7 @@ class HandwritingSampleVisualizer(HandwritingDataBase):
         :param x_label: label of X axis
         :param y_label: label of Y axis
         :param in_air_movement: show in air movements
+        :param on_surface_movement: show on surface movements
         :param fig_show: True/False value to show the figure
         :param save_as: setup save location if you want to save a file
         :return: axis and figure of the plot
@@ -88,8 +132,12 @@ class HandwritingSampleVisualizer(HandwritingDataBase):
         }
 
         # Get strokes
-        stroke_data = sample.get_strokes(on_surface_only=True,
+        stroke_data = sample.get_strokes(on_surface_only=on_surface_movement,
                                          in_air_only=in_air_movement)
+
+        if not stroke_data:
+            self.log(f"No data to plot!")
+            return
 
         # Create figure if axes not inserted
         fig, axis = plt.subplots(1, 1, figsize=fig_kwargs.get("fig_size"))
@@ -104,7 +152,6 @@ class HandwritingSampleVisualizer(HandwritingDataBase):
                             x_label=x_label,
                             y_label=y_label,
                             ax=axis,
-                            in_air_movement=in_air_movement,
                             on_surface_color=on_surface_color,
                             in_air_color=in_air_color)
 
@@ -124,24 +171,23 @@ class HandwritingSampleVisualizer(HandwritingDataBase):
         return axis, plt
 
     @staticmethod
-    def _plot_task(stroke,
-                   x_label=None,
-                   y_label=None,
-                   in_air_movement=False,
-                   on_surface_color=None,
-                   in_air_color=None,
-                   ax=None,
-                   fig=None,
-                   fig_show=True,
-                   save_as=None,
-                   fig_kwargs=None):
+    def _plot_task(
+            stroke,
+            x_label=None,
+            y_label=None,
+            on_surface_color=None,
+            in_air_color=None,
+            ax=None,
+            fig=None,
+            fig_show=True,
+            save_as=None,
+            fig_kwargs=None):
         """
         Plot line plots for input data
 
         :param stroke: instance of handwriting sample
         :param x_label:           label of X axis
         :param y_label:           label of Y axis
-        :param in_air_movement:   show in air movements
         :param on_surface_color:  color of on surface movement
         :param in_air_color:      color of in air movement
         :param ax:                axis to plot on
@@ -207,87 +253,83 @@ class HandwritingSampleVisualizer(HandwritingDataBase):
         # Return
         return ax, plt
 
+    def plot_all_modalities(self, sample, x_label=None, save_as=None):
+        """
+        Plot separate line plots with all modalities of data
 
+        :param sample: instance of handwriting sample
+        :type sample: handwriting_sample.HandwritingSample
+        :param x_label: label of X axis
+        :param save_as: setup save location if you want to save a file
+        """
 
+        # Go over modalities and plot them
+        df_data = sample.data_pandas_dataframe
+        for col in df_data:
+            # Set labels
+            x_label = "sample" if not x_label else x_label
+            y_label = col
 
+            # Plot lines
+            self.plot_line(df_data[col], x_label, y_label, save_as=save_as)
 
-# def plot_all_modalities(data, x_label=None, y_label=None, save_as=None):
-#     """
-#     Plot separate line plots with all modalities of data
-#
-#     :param data: input data loaded from SVC
-#     :param x_label: label of X axis
-#     :param y_label: label of Y axis
-#     :param save_as: setup save location if you want to save a file
-#     """
-#
-#     # Go over modalities and plot them
-#     for col in data.columns:
-#         # Set labels
-#         x_label = col if not x_label else x_label
-#         y_label = "value []" if not y_label else y_label
-#
-#         # Plot lines
-#         plot_line(data[col], x_label, y_label, save_as=save_as)
-#
-#
-# def plot_line(data, x_label, y_label, fig_show=True, save_as=None, fig_kwargs=None):
-#     """
-#     Plot line plots for input data
-#
-#     :param data: input data array
-#     :param x_label: label of X axis
-#     :param y_label: label of Y axis
-#     :param fig_show: True/False value to show the figure
-#     :param save_as: setup save location if you want to save a file
-#     :return: axis and figure of the plot
-#     """
-#
-#     # Prepare the figure settings
-#     fig_kwargs = fig_kwargs if fig_kwargs else {
-#         "fig_size": (15, 4),
-#         "show_ticks": True,
-#         "x_label": x_label,
-#         "y_label": y_label,
-#         "shade": True,
-#         "title": f"{x_label}",
-#         "colors": ["red", "blue", "green"]
-#     }
-#
-#     # Create figure if axes not inserted
-#     fig, ax = plt.subplots(1, 1, figsize=fig_kwargs.get("fig_size"))
-#
-#     # Plot
-#     h = sns.lineplot(data=data, ax=ax)
-#     plt.setp(h.lines, alpha=0.7)
-#
-#     # Set labels
-#     title = fig_kwargs.get("title")
-#     x_label = fig_kwargs.get("x_label")
-#     y_label = fig_kwargs.get("y_label")
-#
-#     ax.set_title(title if title else "", fontname="Times New Roman", fontsize=12)
-#     ax.set_xlabel(x_label if x_label else "", fontname="Times New Roman", fontsize=16)
-#     ax.set_ylabel(y_label if y_label else "", fontname="Times New Roman", fontsize=16)
-#
-#     # Set grid
-#     ax.yaxis.grid(True)
-#     ax.xaxis.grid(True)
-#
-#     # Set tight layout
-#     fig.tight_layout()
-#
-#     # Save the graph
-#     if save_as:
-#         ensure_directory(save_as)
-#         plt.savefig(save_as, bbox_inches="tight", quality=100)
-#
-#     # Show the graph
-#     if fig_show:
-#         plt.show()
-#     else:
-#         plt.close()
-#
-#     return ax, plt
+    @staticmethod
+    def plot_line(data, x_label, y_label, fig_show=True, save_as=None, fig_kwargs=None):
+        """
+        Plot line plots for input data
+
+        :param data: input data array
+        :param x_label: label of X axis
+        :param y_label: label of Y axis
+        :param fig_show: True/False value to show the figure
+        :param save_as: setup save location if you want to save a file
+        :return: axis and figure of the plot
+        """
+
+        # Prepare the figure settings
+        fig_kwargs = fig_kwargs if fig_kwargs else {
+            "fig_size": (15, 4),
+            "show_ticks": True,
+            "x_label": x_label,
+            "y_label": y_label,
+            "shade": True,
+            "title": f"{y_label}",
+            "colors": ["red", "blue", "green"]
+        }
+
+        # Create figure if axes not inserted
+        fig, ax = plt.subplots(1, 1, figsize=fig_kwargs.get("fig_size"))
+
+        # Plot
+        h = sns.lineplot(data=data, ax=ax)
+        plt.setp(h.lines, alpha=0.7)
+
+        # Set labels
+        title = fig_kwargs.get("title")
+        x_label = fig_kwargs.get("x_label")
+        y_label = fig_kwargs.get("y_label")
+
+        ax.set_title(title if title else "", fontname="Times New Roman", fontsize=12)
+        ax.set_xlabel(x_label if x_label else "", fontname="Times New Roman", fontsize=16)
+        ax.set_ylabel(y_label if y_label else "", fontname="Times New Roman", fontsize=16)
+
+        # Set grid
+        ax.yaxis.grid(True)
+        ax.xaxis.grid(True)
+
+        # Set tight layout
+        fig.tight_layout()
+
+        # Save the graph
+        if save_as:
+            plt.savefig(save_as, bbox_inches="tight", quality=100)
+
+        # Show the graph
+        if fig_show:
+            plt.show()
+        else:
+            plt.close()
+
+        return ax, plt
 
 
